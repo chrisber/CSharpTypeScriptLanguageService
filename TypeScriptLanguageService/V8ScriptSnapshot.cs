3115c9c81419d9aaa50877927a9f7a179a747401
chrisber <christian@chrisber.net>
2015-03-14 02:17:48 +0100

    Implemented ScriptSnapshot

--- TypeScriptLanguageService/V8ScriptSnapshot.cs
+++ TypeScriptLanguageService/V8ScriptSnapshot.cs
@@ -3,60 +3,72 @@ using V8.Net;
 
 namespace TypeScriptLanguageService
 {
-    [ScriptObject("ScriptSnapshot", ScriptMemberSecurity.Permanent)]
-    public class V8ScriptSnapshot :  IV8NativeObject
-    {
+	[ScriptObject("ScriptSnapshot", ScriptMemberSecurity.Permanent)]
+	public class V8ScriptSnapshot :  IV8NativeObject
+	{
 
-        IScriptSnapshot scriptSnapshot = null;
-        V8Engine v8Engine = null;
+		IScriptSnapshot scriptSnapshot = null;
+		V8Engine v8Engine = null;
 
-        public V8ScriptSnapshot(V8Engine v8Engine, IScriptSnapshot scriptSnapshot )
-        {
-            this.v8Engine = v8Engine;
-            this.scriptSnapshot = scriptSnapshot;
-        }
-
-        #region IScriptSnapshot implementation
-        public InternalHandle getText(InternalHandle start, InternalHandle end)
-        {
-            return v8Engine.CreateValue(scriptSnapshot.getText(start, end));
-        }
-        public InternalHandle getLength()
-        {
-            return v8Engine.CreateValue(scriptSnapshot.getLength());
-        }
-        public InternalHandle[] getLineStartPositions()
-        {
-            var pos = scriptSnapshot.getLineStartPositions();
-            InternalHandle[] handleArray = new InternalHandle[pos.Length];
-            for (int i = 0; i < pos.Length; i++)
-            {
-                handleArray[i] = v8Engine.CreateValue(pos[i]);
-            }
-            return handleArray;
-        }
-        //@TODO place old snapshot into script???
-        public InternalHandle getChangeRange(InternalHandle oldSnapshot)
-        {
-            //@ TODO IScriptsnapshot needs to be accessible from outside
-
-            return null;
-        }
-        #endregion
+		public V8ScriptSnapshot(V8Engine v8Engine, IScriptSnapshot scriptSnapshot)
+		{
+			this.v8Engine = v8Engine;
+			this.scriptSnapshot = scriptSnapshot;
+			this.Length = scriptSnapshot.getLength().ToString();
+		}
 
 
-        #region IV8NativeObject implementation
+		public string Length { set; get; }
 
-        public void Initialize(V8NativeObject owner, bool isConstructCall, params InternalHandle[] args)
-        {
-        }
 
-        public void Dispose()
-        {
-        }
 
-        #endregion
+		#region IScriptSnapshot implementation
 
-    }
+		public InternalHandle getText(InternalHandle start, InternalHandle end)
+		{
+			return v8Engine.CreateValue(this.scriptSnapshot.getText(start, end));
+		}
+
+		public InternalHandle getLength()
+		{
+			return v8Engine.CreateValue(scriptSnapshot.getLength());
+		}
+
+		public InternalHandle[] getLineStartPositions()
+		{
+			var pos = this.scriptSnapshot.getLineStartPositions();
+			InternalHandle[] handleArray = new InternalHandle[pos.Length];
+			for (int i = 0; i < pos.Length; i++) {
+				handleArray[i] = v8Engine.CreateValue(pos[i]);
+			}
+			return handleArray;
+		}
+
+		public InternalHandle getChangeRange(InternalHandle oldSnapshot)
+		{
+
+			var tChangeRange = scriptSnapshot.getChangeRange(new V8ScriptSnapshotAdapter(oldSnapshot));
+
+			return v8Engine.GetTypeBinder(typeof(TextChangeRange)).CreateObject(tChangeRange);
+
+			return null;
+		}
+
+		#endregion
+
+
+		#region IV8NativeObject implementation
+
+		public void Initialize(V8NativeObject owner, bool isConstructCall, params InternalHandle[] args)
+		{
+		}
+
+		public void Dispose()
+		{
+		}
+
+		#endregion
+
+	}
 }
 
---
+++ TypeScriptLanguageService/V8ScriptSnapshotAdapter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using V8.Net;
+
+namespace TypeScriptLanguageService
+{
+	public class V8ScriptSnapshotAdapter : IScriptSnapshot
+    {
+		InternalHandle snapshot;
+
+        public V8ScriptSnapshotAdapter( InternalHandle snapshot)
+        {
+			this.snapshot = snapshot;
+        }
+
+		#region IScriptSnapshot implementation
+
+		public string getText(int start, int end)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int getLength()
+		{	int length;
+			if(int.TryParse( snapshot.GetProperty("Length"), out length)){
+				return length;
+			}
+				return 0;
+		}
+
+		public int[] getLineStartPositions()
+		{
+			throw new NotImplementedException();
+		}
+
+		public TextChangeRange getChangeRange(IScriptSnapshot oldSnapshot)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+    }
+}
+
